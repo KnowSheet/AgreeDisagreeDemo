dd7da1da6f719c6bd941f306ffdfc1fd70d76ccd
Dima <dmitry.korolev@gmail.com>
2015-03-11 13:44:19 -0700

    Lorempixel images.

--- fetch-lorempixel-images.sh
+++ fetch-lorempixel-images.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
 # Demo use only. Images from lorempixel, all rights [p]reserved.
-for i in `seq 10` ; do curl -s -H "Accept: image/jpg" http://lorempixel.com/400/400/nature/$i/ >$i.jpg ; done
+
+mkdir -p lorempixel
+
+cat <<EOF >lorempixel/LICENSE.txt
+The images from Lorempixel are used, with all the rights [p]reserved.
+EOF
+
+for i in `seq 10` ; do
+  curl -s -H "Accept: image/jpg" http://lorempixel.com/400/400/nature/$i/ >lorempixel/$i.jpg
+done
